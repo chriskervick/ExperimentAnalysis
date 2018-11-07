7140e7b2b70325005b7cc810933e2d195189627b
dmichalak <dennismichalak@DENNISM.PHYS.CMU.EDU>
2018-11-07 15:32:22 -0500

    cleaned up code, added gridlines and reduced font size

--- SPR.py
+++ SPR.py
@@ -2,20 +2,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 from SPRFunctions import *
 
-
-#THE DIRECTORY WITH ALL THE DATA GOES HERE
-
+#Specify data directory, plot title
 dire = "./data/"
-fname = "spr_date_exp"
+title = "Myr Nef on 70:30 POPC:POPG @ 8% and 2.5% Glycerol"
+
+#Specify measured concentrations in micromolar (first should be zero for baseline)
+concs = np.array([0,0.128,0.250,0.509,1.010,2.016,3.972])
+
 data = loader(dire)
+
 #Choose what the default filenames are:
 rinsename = "rinse"
 filename = "conc"
-title = "Myr Nef on 70:30 POPC:POPG @ 8% and 2.5% Glycerol"
-###The various concentrations have to be manually added below in micromolar (first concentation should be zero for baseline)
-concs = np.array([0,0.128,0.250,0.509,1.010,2.016,3.972])
-
-
 
 num = np.size(concs) - 1
 (data,fits,pixels,errors) = Organise(data,num,smoothing=5,filename = filename)
@@ -23,8 +21,6 @@ num = np.size(concs) - 1
 ###Figsize and fontsize here
 fig1 = plt.figure(figsize=(14,10))
 ax1 = fig1.add_subplot(1, 1, 1)
-
-
 
 #THIS AUTOLABELS VIA THE CONCENTRATIONS
 (fig1,ax1,plotdata,plotfits) = Plotter(fig1,ax1,data,fits,pixels,errors,num,filename=filename,ShowFits=False,rinselimit=0)
@@ -37,24 +33,17 @@ try:
 except IndexError:
     print("Final rinse is not plotted if ShowFits==True")
 
-
 ax1.set_title(title)
+ax1.legend(prop={'size': 12},loc=0)
 
-ax1.legend(prop={'size': 12})
+ax1.grid(axis='y',alpha=0.4)
 
-
-
-
+#Set the font size of labels
 for item in ([ax1.title, ax1.xaxis.label, ax1.yaxis.label] +
              ax1.get_xticklabels() + ax1.get_yticklabels()):
-    item.set_fontsize(20)
-
-
-
+    item.set_fontsize(16)
 
 plt.savefig(fname+'.pdf')
 plt.savefig(name+'.png')
 
-ax1.set_title(title)
-
-FitToLangmuir(concs,pixels,errors,title)
+#FitToLangmuir(concs,pixels,errors,title)
